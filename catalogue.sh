65bd8b3a22b0aff64f6cbb7862b0d9f33ef243bd
Sundhar <pujarihemasundhar@gmail.com>
2025-09-11 12:11:05 +0530

    changes in catalogue.sh file

--- catalogue.sh
+++ catalogue.sh
@@ -17,7 +17,7 @@ mkdir -p $LOGS_FOLDER
 echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
 # check the user has root priveleges or not
-if [ $USERID -ne 0 ]
+if [ $userid -ne 0 ]
 then
     echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
     exit 1 #give other than 0 upto 127
